0a6186b48710581941967172906b99c4eef6dc83
D-Foschini <daniel.foschini@sptech.school>
2025-10-08 17:11:47 -0300

    Query Tabelas V1 Sprint2

---
+++ Projeto/Banco de Dados/Query BD Grupo02 Sprint2.sql
@@ -0,0 +1,63 @@
+-- banco de dados grupo 02 adamot
+
+CREATE DATABASE adamot;
+
+USE adamot;
+
+CREATE TABLE shopping(
+	idShopping INT PRIMARY KEY AUTO_INCREMENT,
+	nomeShopping VARCHAR(45),
+    numeroTel CHAR(11)
+);
+
+CREATE TABLE cadastro(
+	idCadastro INT PRIMARY KEY AUTO_INCREMENT,
+    dtCadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+	usuario VARCHAR(45),
+    email VARCHAR(45) UNIQUE,
+    cpf CHAR(11) UNIQUE,
+    dtNasc DATE,
+    primeiroNome VARCHAR(45),
+    sobreNome VARCHAR(45),
+    senha VARCHAR(45),
+    cargo VARCHAR(45),
+    fkShopping INT NOT NULL,
+    CONSTRAINT fk_shop_cad FOREIGN KEY (fkShopping) REFERENCES shopping(idShopping)
+);
+
+CREATE TABLE auditoria(
+	idAuditoria INT PRIMARY KEY AUTO_INCREMENT,
+    fkCadastro INT,
+    CONSTRAINT pkAuditCad PRIMARY KEY (idAuditoria,fkCadastro),
+    CONSTRAINT fk_Aud_Cad FOREIGN KEY (fkCadastro) REFERENCES cadastro(idCadastro),
+    acesso VARCHAR(45),
+    senha VARCHAR(45),
+    dtAcesso DATETIME
+);
+
+CREATE TABLE entrada (
+	idEntrada INT PRIMARY KEY AUTO_INCREMENT,
+    fkShopping INT,
+    CONSTRAINT pkEntShop PRIMARY KEY (idEntrada,fkShopping),
+    CONSTRAINT fk_Ent_Shop FOREIGN KEY (fkShopping) REFERENCES shopping(idShopping),
+    nomeEntrada VARCHAR(45)
+);
+
+CREATE TABLE sensor (
+	idSensor INT PRIMARY KEY AUTO_INCREMENT,
+    quantidade INT,
+    dtCaptura DATETIME,
+    fkEntrada INT,
+    CONSTRAINT fk_Sen_Ent FOREIGN KEY (fkEntrada) REFERENCES entrada(idEntrada)
+);
+
+CREATE TABLE endereco(
+	idEndereco INT PRIMARY KEY AUTO_INCREMENT,
+    cep CHAR(8),
+    bairro VARCHAR(45),
+    cidade CHAR(2),
+    uf CHAR(2),
+    numero CHAR(11),
+    fkShopping INT,
+    CONSTRAINT fk_End_Shop FOREIGN KEY (fkShopping) REFERENCES shopping(idShopping)
+);
