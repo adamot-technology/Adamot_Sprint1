d019f180c7c88c0944886f211c1cef812cc692c7
Robson Guilherme <robson.almeida@sptech.school>
2025-10-24 02:50:01 -0300

    Update Query BD Grupo02 Sprint2.sql
    correção de tabelas após alteração da modelagem.

--- Projeto/Banco de Dados/Query BD Grupo02 Sprint2.sql
+++ Projeto/Banco de Dados/Query BD Grupo02 Sprint2.sql
@@ -1,4 +1,3 @@
-
 -- banco de dados grupo 02 adamot
 
 CREATE DATABASE adamot;
@@ -8,32 +7,27 @@ USE adamot;
 CREATE TABLE shopping(
 	idShopping INT PRIMARY KEY AUTO_INCREMENT,
 	nomeShopping VARCHAR(45),
-    numeroTel CHAR(11)
+    numeroTel CHAR(11),
+    cnpj char(14),
+    token char(6)
 );
 
-CREATE TABLE cadastro(
-	idCadastro INT PRIMARY KEY AUTO_INCREMENT,
-    dtCadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
-	usuario VARCHAR(45),
+CREATE TABLE usuario(
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(45) UNIQUE,
     cpf CHAR(11) UNIQUE,
-    dtNasc DATE,
-    primeiroNome VARCHAR(45),
-    sobreNome VARCHAR(45),
     senha VARCHAR(45),
-    cargo VARCHAR(45),
     fkShopping INT NOT NULL,
     CONSTRAINT fk_shop_cad FOREIGN KEY (fkShopping) REFERENCES shopping(idShopping)
 );
 
 CREATE TABLE auditoria(
-	idAuditoria INT,
-    fkCadastro INT,
-    CONSTRAINT pkAuditCad PRIMARY KEY (idAuditoria,fkCadastro),
-    CONSTRAINT fk_Aud_Cad FOREIGN KEY (fkCadastro) REFERENCES cadastro(idCadastro),
+	idAuditoria INT AUTO_INCREMENT,
     acesso VARCHAR(45),
     senha VARCHAR(45),
-    dtAcesso DATETIME
+	fkCadastro INT,
+    CONSTRAINT pkAuditCad PRIMARY KEY (idAuditoria,fkCadastro),
+    CONSTRAINT fk_Aud_Cad FOREIGN KEY (fkCadastro) REFERENCES usuario(idUsuario)
 );
 
 CREATE TABLE entrada (
@@ -44,31 +38,43 @@ CREATE TABLE entrada (
     nomeEntrada VARCHAR(45)
 );
 
-CREATE TABLE sensor (
+CREATE TABLE sensor(
 	idSensor INT PRIMARY KEY AUTO_INCREMENT,
-    quantidade INT,
-    dtCaptura DATETIME,
+    modelo VARCHAR (45),
+    dtInstalacao DATETIME,
+    dtManutencao DATETIME,
     fkEntrada INT,
     CONSTRAINT fk_Sen_Ent FOREIGN KEY (fkEntrada) REFERENCES entrada(idEntrada)
 );
 
 CREATE TABLE endereco(
-	idEndereco INT,
-	fkShopping INT,
-    constraint pkEndereco_Shopping
-    primary key (idEndereco, fkShopping),
-    cep CHAR(8),
+	idEndereco INT AUTO_INCREMENT,
+    cep CHAR(9),  
     bairro VARCHAR(45),
     cidade CHAR(2),
     uf CHAR(2),
     numero CHAR(11),
+    fkShopping INT,
+    CONSTRAINT pk_End_Shop PRIMARY KEY (idEndereco,fkShopping),
     CONSTRAINT fk_End_Shop FOREIGN KEY (fkShopping) REFERENCES shopping(idShopping)
-);
+);  
 
 CREATE TABLE registro(
-idRegistro INT PRIMARY KEY AUTO_INCREMENT,
-registroSensor INT NOT NULL,
+idRegistro INT AUTO_INCREMENT,
+valor INT,
 dtHora DATETIME,
 fkSensor INT,
-CONSTRAINT fk_Registro_Sensor FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor)
+CONSTRAINT pk_Reg_Sen PRIMARY KEY (idRegistro,fkSensor),
+CONSTRAINT fk_Reg_Sen FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor)
 );
+
+CREATE TABLE Fale_Conosco(
+idFaleConosco INT PRIMARY KEY AUTO_INCREMENT,
+nomeFac VARCHAR (45),
+emailFac VARCHAR (45) UNIQUE,
+mensagem VARCHAR (400),
+fkRegistro INT,
+CONSTRAINT pk_Fac_reg FOREIGN KEY (fkRegistro) REFERENCES registro(idRegistro)
+);
+
+DROP DATABASE adamot;
